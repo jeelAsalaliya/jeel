4952ba43732ee09175bb3c60926f8b694b99ba13
jeelAsalaliya <aslalaliyajeel@gmail.com>
2024-07-14 12:26:34 +0530

    jeel
    good

--- Card.jsx
+++ Card.jsx
@@ -1,77 +1,29 @@
 import React from "react";
 
-export default function Card() {
+function Card({ username, btntext }) {
+    console.log(username);
     return (
-        <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src="D:\GOD IMAGES\GANESH.jpg" alt="product_image1" />
-            </a>
-            <div className="px-5 pb-5">
-                <a href="/">
-                    <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport
-                    </h5>
-                </a>
-                <div className="flex items-center mt-2.5 mb-5">
-                    <svg
-                        className="w-4 h-4 text-yellow-300 mr-1"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 22 20"
-                    >
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                        className="w-4 h-4 text-yellow-300 mr-1"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 22 20"
-                    >
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                        className="w-4 h-4 text-yellow-300 mr-1"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 22 20"
-                    >
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                        className="w-4 h-4 text-yellow-300 mr-1"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 22 20"
-                    >
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <svg
-                        className="w-4 h-4 text-gray-200 dark:text-gray-600"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="currentColor"
-                        viewBox="0 0 22 20"
-                    >
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z" />
-                    </svg>
-                    <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
-                        4.0
-                    </span>
+        <>
+            <div className="relative h-[400px] w-[300px] rounded-md">
+                <img
+                    src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
+                    alt="AirMax Pro"
+                    class="z-0 h-full w-full rounded-md object-cover" height={500} width={300}
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+                <div className="absolute bottom-4 left-4 text-left">
+                    <h1 className="text-lg font-semibold text-white">{username}</h1>
+                    <p className="mt-2 text-sm text-gray-300">
+                        An introduction for an essay or research paper is the first paragraph, which explains the topic and prepares the reader for the rest of the work. Because it’s responsible for both the reader’s first impression and setting the stage for the rest of the work, the introduction paragraph is arguably the most important paragraph in the work.
+                        An introduction for an essay or research paper is the first paragraph, which explains the topic and prepares the reader for the rest of the work. Because it’s responsible for both the reader’s first impression and setting the stage for the rest of the work, the introduction paragraph is arguably the most important paragraph in the work.
+                    </p>
+                    <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
+                        {btntext || 'click me'} →
+                    </button>
                 </div>
-                <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
-                    <a
-                        href="/"
-                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                    >
-                        Add to cart
-                    </a>
-                </div>
-            </div>
-        </div>
-    );
+            </div ><br />
+        </>
+    )
 }
+
+export default Card;
--- Header.jsx
+++ Header.jsx
@@ -1,73 +1,84 @@
-import React from 'react'
-import { Container, Logo, LogoutBtn } from '../index'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-
-function Header() {
-    const authStatus = useSelector((state) => state.auth.status)
-    const navigate = useNavigate()
-
-    const navItems = [
-        {
-            name: 'Home',
-            slug: "/",
-            active: true
-        },
-        {
-            name: "Login",
-            slug: "/login",
-            active: !authStatus,
-        },
-        {
-            name: "Signup",
-            slug: "/signup",
-            active: !authStatus,
-        },
-        {
-            name: "All Posts",
-            slug: "/all-posts",
-            active: authStatus,
-        },
-        {
-            name: "Add Post",
-            slug: "/add-post",
-            active: authStatus,
-        },
-    ]
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 
+export default function header() {
     return (
-        <header className='py-3 shadow bg-gray-500'>
-            <Container>
-                <nav className='flex'>
-                    <div className='mr-4'>
-                        <Link to='/'>
-                            <Logo width='70px' />
-
+        <header className="shadow sticky z-50 top-0">
+            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+                <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+                    <Link to="/" className="flex items-center">
+                        <img
+                            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+                            className="mr-3 h-12"
+                            alt="Logo"
+                        />
+                    </Link>
+                    <div className="flex items-center lg:order-2">
+                        <Link
+                            to="#"
+                            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Log in
+                        </Link>
+                        <Link
+                            to="#"
+                            className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
+                        >
+                            Get started
                         </Link>
                     </div>
-                    <ul className='flex ml-auto'>
-                        {navItems.map((item) =>
-                            item.active ? (
-                                <li key={item.name}>
-                                    <button
-                                        onClick={() => navigate(item.slug)}
-                                        className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-                                    >{item.name}</button>
-                                </li>
-                            ) : null
-                        )}
-                        {authStatus && (
+                    <div
+                        className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+                        id="mobile-menu-2"
+                    >
+                        <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
-                                <LogoutBtn />
+                                <NavLink to="/"
+                                    className={({ isActive }) =>
+                                        `block py - 2 pr - 4 pl - 3 duration - 200 ${isActive ? "text-orange-700" : "text-gray-700"} border - b border - gray - 100 hover: bg - gray - 50 lg: hover: bg - transparent lg: border - 0 hover: text - orange - 700 lg: p - 0`
+                                    }
+                                >
+                                    Home
+                                </NavLink>
                             </li>
-                        )}
-                    </ul>
-                </nav>
-            </Container>
-        </header>
-    )
-}
 
-export default Header
+                            <li>
+                                <NavLink to="/about"
+                                    className={({ isActive }) =>
+                                        `block py - 2 pr - 4 pl - 3 duration - 200 ${isActive ? "text-orange-700" : "text-gray-700"} border - b border - gray - 100 hover: bg - gray - 50 lg: hover: bg - transparent lg: border - 0 hover: text - orange - 700 lg: p - 0`
+                                    }
+                                >
+                                    about
+                                </NavLink>
+                            </li>
+
+                            <li>
+                                <NavLink to="/github"
+                                    className={({ isActive }) =>
+                                        `block py - 2 pr - 4 pl - 3 duration - 200 ${isActive ? "text-orange-700" : "text-gray-700"} border - b border - gray - 100 hover: bg - gray - 50 lg: hover: bg - transparent lg: border - 0 hover: text - orange - 700 lg: p - 0`
+                                    }
+                                >
+                                    github
+                                </NavLink>
+                            </li>
+
+
+                            <li>
+                                <NavLink to="/contact"
+                                    className={({ isActive }) =>
+                                        `block py - 2 pr - 4 pl - 3 duration - 200 ${isActive ? "text-orange-700" : "text-gray-700"} border - b border - gray - 100 hover: bg - gray - 50 lg: hover: bg - transparent lg: border - 0 hover: text - orange - 700 lg: p - 0`
+                                    }
+                                >
+                                    contact
+                                </NavLink>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
